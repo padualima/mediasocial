bf34b7b40b9779a58dfec168fbe9ce01e10ae95a
Pádua Lima <padualima.dev@gmail.com>
2020-01-24 04:43:29 -0300

    version 1.8, with searching to date, but not validation, dependences futures, so, go now refatoring the controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     prepare_form
-    @posts = Post.order('created_at desc').page(params[:page]).per(40)
+    @posts = Post.order('created_at desc').page().per(40)
   end
 
   def search
@@ -12,10 +12,21 @@ class HomeController < ApplicationController
           @posts = Post.search(search_params[:q]).page(params[:page]).per(20)
         end
         if params[:lists].present?
-          return @posts = Post.search_lists(search_params[:lists]).page(params[:page]).per(20)
+          return @posts = Post.search_lists(search_params[:lists]).page().per(20)
         end
         if params[:accounts].present?
           return @posts = Post.search_account(search_params[:accounts]).page(params[:page]).per(20)
+        end
+
+        par_first = params[:date_first].values
+        par_end = params[:date_end].values
+        if (par_first[0].present? && par_first[1].present? && par_first[2].present?) &&
+          (par_end[0].present? && par_end[1].present? && par_end[2].present?)
+          date_first = par_first.reverse.join('-')
+          date_end = par_end.reverse.join('-')
+          @posts = Post.where("created_at between (?) and (?)", date_first, date_end).order('created_at desc')
+
+          # RESOLVER O redirect_to root_path
         end
       }
     end
@@ -23,13 +34,12 @@ class HomeController < ApplicationController
 
   private
 
-
   def prepare_form
     @lists = List.all.map { |l| [l.description, l.id] }
     @accounts = Account.all.map { |ac| [ac.description, ac.id] }
   end
 
   def search_params
-    params.permit(:q, :lists, :accounts)
+    params.permit(:q, :lists, :accounts, :date_first, :date_end)
   end
 end
