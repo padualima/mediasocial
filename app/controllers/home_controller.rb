ebfe11ac2b42c912171c5c7ce1ff07e21924907e
Pádua Lima <padualima.dev@gmail.com>
2020-01-24 04:53:33 -0300

    controller factored

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,38 +1,52 @@
 class HomeController < ApplicationController
   def index
     prepare_form
-    @posts = Post.order('created_at desc').page().per(40)
+    # for call of kind HTML
+    @posts = Post.order('created_at desc').page(params[:page]).per(40)
   end
 
   def search
     prepare_form
     respond_to do |format|
       format.js {
-        if params[:q].present?
-          @posts = Post.search(search_params[:q]).page(params[:page]).per(20)
-        end
-        if params[:lists].present?
-          return @posts = Post.search_lists(search_params[:lists]).page().per(20)
-        end
-        if params[:accounts].present?
-          return @posts = Post.search_account(search_params[:accounts]).page(params[:page]).per(20)
-        end
-
-        par_first = params[:date_first].values
-        par_end = params[:date_end].values
-        if (par_first[0].present? && par_first[1].present? && par_first[2].present?) &&
-          (par_end[0].present? && par_end[1].present? && par_end[2].present?)
-          date_first = par_first.reverse.join('-')
-          date_end = par_end.reverse.join('-')
-          @posts = Post.where("created_at between (?) and (?)", date_first, date_end).order('created_at desc')
-
-          # RESOLVER O redirect_to root_path
-        end
+        search_by_input
+        search_by_list
+        search_by_account_post
+        search_by_date
       }
     end
   end
 
   private
+
+  def search_by_input
+    if params[:q].present?
+      @posts = Post.search(search_params[:q])
+    end
+  end
+
+  def search_by_list
+    if params[:lists].present?
+      return @posts = Post.search_lists(search_params[:lists])
+    end
+  end
+
+  def search_by_account_post
+    if params[:accounts].present?
+      return @posts = Post.search_account(search_params[:accounts])
+    end
+  end
+
+  def search_by_date
+    par_first = params[:date_first].values
+    par_end = params[:date_end].values
+    if (par_first[0].present? && par_first[1].present? && par_first[2].present?) &&
+      (par_end[0].present? && par_end[1].present? && par_end[2].present?)
+      date_first = par_first.reverse.join('-')
+      date_end = par_end.reverse.join('-')
+      @posts = Post.where("created_at between (?) and (?)", date_first, date_end).order('created_at desc')
+    end
+  end
 
   def prepare_form
     @lists = List.all.map { |l| [l.description, l.id] }
